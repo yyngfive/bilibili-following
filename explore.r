1e40caf94bc4706c870bd7d9ec3a1a950339842b
yyngfive <chenhye5@outlook.com>
2022-12-19 18:35:19 +0800

    画了些图

--- explore.r
+++ explore.r
@@ -1,7 +1,18 @@
 library(tidyverse)
 library(RColorBrewer)
 library(ggsci)
-df <- read.csv("./UPs.csv", colClasses = c("uid" = "character", "time" = "Date", "special" = "factor", "first" = "Date", "newest" = "Date", "update" = "factor", "watch" = "factor"), encoding = "UTF-8")
+df <- read.csv("./UPs.csv",
+    colClasses = c(
+        "uid" = "character",
+        "time" = "Date",
+        "special" = "factor",
+        "first" = "Date",
+        "newest" = "Date",
+        "update" = "factor",
+        "watch" = "factor"
+    ),
+    encoding = "UTF-8"
+)
 View(df)
 df <- as_tibble(df)
 str(df)
@@ -51,14 +62,170 @@ num_one <- df %>%
     .[1] - num_two
 num_two
 num_one
-pie(c(num_one, num_two), labels = c("", "在两个分区发布视频"), border = "white", col = pal_npg("nrc", alpha = 0.7)(2))
+
+library(hrbrthemes)
+
+pie(c(num_one, num_two),
+    labels = c("", "在两个分区发布视频"),
+    border = "white",
+    col = pal_npg("nrc", alpha = 0.7)(2)
+)
 
 ggplot(data = two_fields, aes(x = fans_level, fill = fans_level)) +
     geom_bar() +
     scale_fill_npg()
 
 library(treemap)
-treemap(df, index = "single_field", vSize = "fans")
+treemap(table(df$single_field) %>%
+    as.data.frame(),
+index = "Var1", vSize = "Freq"
+)
 
-two_fields_56  <-  two_fields %>% filter(fans_level == 5 | fans_level == 6)
+two_fields_56 <- two_fields %>% filter(fans_level == 5 | fans_level == 6)
 two_fields_56
+main_fields <- two_fields_56$single_field %>%
+    as.character() %>%
+    as.factor()
+second_fields <- str_split(two_fields_56$field,
+    ",",
+    simplify = T
+)[, 2] %>%
+    as.factor()
+
+data_56 <- expand.grid(x = levels(main_fields), y = levels(second_fields))
+data_56$z <- table(two_fields_56$field) %>%
+    .[paste(data_56$x, data_56$y, sep = ",")] %>%
+    as.numeric()
+data_56$z[is.na(data_56$z)] <- 0
+data_56 <- as_tibble(data_56)
+data_56
+ggplot(data_56, aes(x = x, y = y, fill = z)) +
+    geom_tile() +
+    scale_fill_material("blue")
+
+ggplot(table(two_fields_56$field) %>%
+    as.data.frame(), aes(x = reorder(Var1, Freq), y = Freq)) +
+    geom_segment(aes(
+        x = reorder(Var1, Freq),
+        xend = reorder(Var1, Freq),
+        y = 0,
+        yend = Freq
+    ),
+    col = "grey"
+    ) +
+    geom_point(color = "orange", size = 4) +
+    theme_ipsum()
+
+library(ggradar)
+
+table_to_radar <- function(table) {
+    table %>%
+        as.data.frame.array() %>%
+        t() %>%
+        apply(2, function(x) x / sum(.)) %>%
+        as.data.frame() %>%
+        t() %>%
+        as_tibble()
+}
+
+table(df$single_field) %>%
+    table_to_radar() %>%
+    ggradar() +
+    scale_color_npg()
+
+field_video <- c("电影", "纪录片", "影视", "动画", "国创")
+field_entertainment <- c("音乐", "舞蹈", "游戏", "鬼畜", "娱乐")
+field_knowledge <- c("知识", "科技", "资讯")
+field_life <- c("生活", "美食", "动物圈", "汽车", "时尚", "运动")
+
+df$big_field <- df %>%
+    select(single_field) %>%
+    apply(1, function(x) {
+        if (x %in% field_video) {
+            res <- "vedio"
+        } else if (x %in% field_entertainment) {
+            res <- "entertainment"
+        } else if (x %in% field_knowledge) {
+            res <- "knowledge"
+        } else if (x %in% field_life) {
+            res <- "life"
+        } else {
+            res <- "none"
+        }
+        return(res)
+    })
+
+df$big_field %>%
+    table() %>%
+    table_to_radar() %>%
+    select(!none) %>%
+    ggradar() +
+    scale_color_npg()
+
+df %>%
+    select(fans, big_field) %>%
+    filter(big_field != "none") %>%
+    ggplot(aes(x = big_field, y = log10(fans), fill = big_field)) +
+    geom_boxplot() +
+    theme_ipsum() +
+    scale_fill_npg()
+
+df$view_mean <- df$view / df$total
+df$likes_mean <- df$likes / df$total
+df$view_mean[is.infinite(df$view_mean)] <- 0
+df$view_mean[is.nan(df$view_mean)] <- 0
+df$likes_mean[is.infinite(df$likes_mean)] <- 0
+df %>% ggplot(aes(x = log(view), y = log(likes))) +
+    geom_point()
+df %>% ggplot(aes(x = log(view), y = log(likes), col = big_field)) +
+    geom_point() +
+    theme_ipsum() +
+    scale_color_npg()
+
+df %>% ggplot(aes(x = log(fans), y = log(view))) +
+    geom_point()
+
+df %>% ggplot(aes(x = log10(total), y = log10(view_mean), col = big_field)) +
+    geom_point() +
+    theme_ipsum() +
+    scale_color_npg()
+
+df %>% ggplot(aes(x = fans, y = view_mean)) +
+    geom_point()
+
+
+
+field.update <- expand.grid(x = levels(as.factor(df$big_field)), y = levels(df$update))
+field.update
+field.update$z <- table(df$big_field, df$update) %>% as.vector()
+
+
+field.update$z <- scale(field.update["z"])
+ggplot(field.update, aes(x = x, y = y, fill = z)) +
+    geom_tile() +
+    scale_fill_material("blue")
+
+table(df$big_field, df$update) %>%
+    as.data.frame() %>%
+    ggplot(aes(x = Var2, y = Freq, fill = Var1)) +
+    geom_bar(position = "stack", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
+table(df$big_field, df$update) %>%
+    as.data.frame() %>%
+    ggplot(aes(x = Var2, y = Freq, fill = Var1)) +
+    geom_bar(position = "fill", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
+table(df$big_field, df$update) %>%
+    as.data.frame() %>%
+    ggplot(aes(x = Var1, y = Freq, fill = Var2)) +
+    geom_bar(position = "stack", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
+table(df$big_field, df$update) %>%
+    as.data.frame() %>%
+    ggplot(aes(x = Var1, y = Freq, fill = Var2)) +
+    geom_bar(position = "fill", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
