d87c42f26be5daac01be8fd25782ef3fbc4fb308
yyngfive <chenhye5@outlook.com>
2022-12-22 12:24:39 +0800

    增加更多图

--- explore.r
+++ explore.r
@@ -76,9 +76,10 @@ ggplot(data = two_fields, aes(x = fans_level, fill = fans_level)) +
     scale_fill_npg()
 
 library(treemap)
-treemap(table(df$single_field) %>%
-    as.data.frame(),
-index = "Var1", vSize = "Freq"
+treemap(
+    table(df$single_field) %>%
+        as.data.frame(),
+    index = "Var1", vSize = "Freq"
 )
 
 two_fields_56 <- two_fields %>% filter(fans_level == 5 | fans_level == 6)
@@ -105,13 +106,14 @@ ggplot(data_56, aes(x = x, y = y, fill = z)) +
 
 ggplot(table(two_fields_56$field) %>%
     as.data.frame(), aes(x = reorder(Var1, Freq), y = Freq)) +
-    geom_segment(aes(
-        x = reorder(Var1, Freq),
-        xend = reorder(Var1, Freq),
-        y = 0,
-        yend = Freq
-    ),
-    col = "grey"
+    geom_segment(
+        aes(
+            x = reorder(Var1, Freq),
+            xend = reorder(Var1, Freq),
+            y = 0,
+            yend = Freq
+        ),
+        col = "grey"
     ) +
     geom_point(color = "orange", size = 4) +
     theme_ipsum()
@@ -125,7 +127,8 @@ table_to_radar <- function(table) {
         apply(2, function(x) x / sum(.)) %>%
         as.data.frame() %>%
         t() %>%
-        as_tibble()
+        as_tibble() %>%
+        add_column(group = "Radar", .before = colnames(.)[1])
 }
 
 table(df$single_field) %>%
@@ -142,7 +145,7 @@ df$big_field <- df %>%
     select(single_field) %>%
     apply(1, function(x) {
         if (x %in% field_video) {
-            res <- "vedio"
+            res <- "video"
         } else if (x %in% field_entertainment) {
             res <- "entertainment"
         } else if (x %in% field_knowledge) {
@@ -159,6 +162,7 @@ df$big_field %>%
     table() %>%
     table_to_radar() %>%
     select(!none) %>%
+    rename(泛影视 = video, 泛娱乐 = entertainment, 泛知识 = knowledge, 泛生活 = life) %>%
     ggradar() +
     scale_color_npg()
 
@@ -190,9 +194,36 @@ df %>% ggplot(aes(x = log10(total), y = log10(view_mean), col = big_field)) +
     theme_ipsum() +
     scale_color_npg()
 
-df %>% ggplot(aes(x = fans, y = view_mean)) +
+df %>% ggplot(aes(x = log10(fans), y = log10(view_mean))) +
     geom_point()
 
+ggplot(df, aes(x = fans_level, y = view_mean, fill = fans_level)) +
+    geom_boxplot() +
+    theme_ipsum() +
+    scale_fill_npg()
+library(gt)
+library(gtExtras)
+library(formattable)
+df[order(df$view_mean, decreasing = T), ] %>%
+    select(name, view_mean, likes_mean, fans, total) %>%
+    .[1:10, ] %>%
+    gt() %>%
+    cols_label(
+        name = md("UP主"),
+        view_mean = md("平均播放量"),
+        likes_mean = md("平均点赞数"),
+        fans = md("粉丝数"),
+        total = md("视频总数")
+    ) %>%
+    fmt_integer(columns = c(view_mean, likes_mean, fans)) %>%
+    tab_header(md("关注的UP主平均播放量前十")) %>%
+    gt_highlight_rows(rows = 1, font_weight = "normal") %>%
+    gt_theme_espn()
+df[order(df$likes_mean, decreasing = T), ] %>%
+    select(name, view_mean, view, likes, likes_mean, fans, total) %>%
+    .[1:10, ] %>%
+    gt() %>%
+    gt_theme_espn()
 
 
 field.update <- expand.grid(x = levels(as.factor(df$big_field)), y = levels(df$update))
@@ -229,3 +260,63 @@ table(df$big_field, df$update) %>%
     geom_bar(position = "fill", stat = "identity") +
     theme_ipsum() +
     scale_fill_npg()
+
+df %>% filter(big_field == "none")
+
+library(lubridate)
+
+table <- df["time"] %>%
+    apply(1, year) %>%
+    table(df$big_field)
+colnames(table) <- c("Entertainment", "Knowledge", "Life", "None", "Videos")
+table %>%
+    as.data.frame() %>%
+    rename_with(function(x) {
+        return(c("Year", "Fields", "Freq"))
+    }) %>%
+    ggplot(aes(x = Year, y = Freq, fill = Fields)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    xlab("Year") +
+    ylab("Count") +
+    theme_ipsum() +
+    scale_fill_npg()
+df["time"] %>%
+    apply(1, year) %>%
+    table(df$big_field) %>%
+    as.data.frame() %>%
+    rename_with(function(x) {
+        return(c("Year", "Field", "Freq"))
+    }) %>%
+    ggplot(aes(x = Year, y = Freq, fill = Field)) +
+    geom_bar(position = "stack", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
+
+df["time"] %>%
+    apply(1, year) %>%
+    table(df$update) %>%
+    as.data.frame() %>%
+    rename_with(function(x) {
+        return(c("Year", "Update", "Freq"))
+    }) %>%
+    ggplot(aes(x = Year, y = Freq, fill = Update)) +
+    geom_bar(position = "fill", stat = "identity") +
+    theme_ipsum() +
+    scale_fill_npg()
+
+year_color <- colorRampPalette(pal_npg("nrc")(10))(15)
+
+df["first"] %>%
+    apply(1, year) %>%
+    table() %>%
+    as.data.frame() %>%
+    rename_with(function(x) {
+        return(c("Year", "Freq"))
+    }) %>%
+    filter(Year != 1970) %>%
+    ggplot(aes(x = Year, y = Freq)) +
+    geom_bar(stat = "identity", fill = "orange") +
+    theme_ipsum()
+# scale_fill_manual(values=year_color)
+
+df %>% filter(year(first) == 2010)
