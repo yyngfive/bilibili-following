f57397d565d2a1d3c2591e58a1664affcb9e8c91
yyngfive <chenhye5@outlook.com>
2022-12-24 15:55:26 +0800

    update

--- index.py
+++ index.py
@@ -1,7 +1,50 @@
-from fastapi import FastAPI
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
-app = FastAPI() # 创建 api 对象
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 
-@app.get('/') # 根路由
-async def root():
-    return {'message':'Hello, FastAPI'}
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+
+colors = {
+    'background':'#111111',
+    'text':'#7FDBFF',
+}
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 20, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor = colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color = colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor':colors['background']},children=[
+    html.H1(children='Hello Dash',style={
+        'textAlign':'center',
+        'color':colors['text']}),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    ''',style={
+        'textAlign':'center',
+        'color':colors['text']}),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True,dev_tools_hot_reload=True)
